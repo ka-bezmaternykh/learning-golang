f11de86cad4c15546365903d6d353e6493291f21
Kirill Bezmaternykh <ka.bezmaternykh@gmail.com>
2024-11-14 21:13:33 +0800

    Refactor greeting with using of env vars

--- hello.go
+++ hello.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, world")
+	greeting()
+}
+
+func greeting() {
+	hostname, error := os.Hostname()
+	if error != nil {
+		fmt.Println(error)
+		return
+	}
+	username := os.Getenv("USERNAME")
+	if username == "" {
+		fmt.Println("Username not found")
+		username = "Anonymous"
+	}
+	message := "Hello, " + username + ". I am running on " + hostname
+	fmt.Println(message)
 }
